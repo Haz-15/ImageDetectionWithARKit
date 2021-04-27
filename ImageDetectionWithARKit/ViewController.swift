5f383a0e32c71f94fa72a354b8bbc906917eda6b
Haz-15 <Ab2388@njit.edu>
2021-04-27 12:32:42 -0400

    Completed Class app -Only works for eve card right now

--- ImageDetectionWithARKit/ViewController.swift
+++ ImageDetectionWithARKit/ViewController.swift
@@ -19,25 +19,82 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
+    
+    //Image Detection -> ANCHOR
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        //Link to ARResource Group
+        
+        //if-let guard-let
+        if let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+        //Tracking
+        configuration.trackingImages = referenceImages
+        
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Images successfully loaded")
+        }
         // Run the view's session
         sceneView.session.run(configuration)
     }
+    
+    
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let node = SCNNode()
+        
+        if let anchor = anchor as? ARImageAnchor {
+            
+            let plane = SCNPlane(width: anchor.referenceImage.physicalSize.width, height: anchor.referenceImage.physicalSize.height)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            
+            node.addChildNode(planeNode)
+            
+            if anchor.referenceImage.name == "evee-card" {
+                
+                //ROOT NODE -> MODEL NODE -> MODEL NODE
+                
+                
+                if let pokeScene = SCNScene(named : "art.scnassets/eevee.scn") {
+                    
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        
+                        
+                        
+                        pokeNode.eulerAngles.x = .pi / 2
+                        
+                        planeNode.addChildNode(pokeNode)
+                    }
+                    
+                }
+                
+            }
+        }
+        
+        
+        return node
+    }
+    
+    
+    
+    
+    
+    
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
